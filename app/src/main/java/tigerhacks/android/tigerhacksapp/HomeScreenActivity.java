30bd605231bcbeaf5503799a6d24df6c4375e1d8
Connor Penrod <cfpvf9@mail.missouri.edu>
2018-03-22 23:36:03 -0500

    Set up XML color attributes for navbar

--- app/src/main/java/tigerhacks/android/tigerhacksapp/HomeScreenActivity.java
+++ app/src/main/java/tigerhacks/android/tigerhacksapp/HomeScreenActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.TextView;
 
 import java.net.URI;
