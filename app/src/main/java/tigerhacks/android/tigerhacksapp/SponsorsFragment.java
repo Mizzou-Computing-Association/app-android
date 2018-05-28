aedf412619fbbc373c292594807c1eb3562df146
Connor Penrod <cfpvf9@mail.missouri.edu>
2018-05-27 22:30:08 -0500

    More work on sponsors

--- app/src/main/java/tigerhacks/android/tigerhacksapp/SponsorsFragment.java
+++ app/src/main/java/tigerhacks/android/tigerhacksapp/SponsorsFragment.java
@@ -93,7 +93,6 @@ public class SponsorsFragment extends Fragment {
         {
             Log.i("TEST5", "NavFix worked, somewhat.");
         }
-        Log.i("WTF", "WTF is happening.");
         return layout;
     }
 
@@ -126,10 +125,6 @@ public class SponsorsFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
