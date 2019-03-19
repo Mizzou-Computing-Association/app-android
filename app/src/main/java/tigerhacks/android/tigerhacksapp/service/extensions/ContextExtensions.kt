22f6ef074c3a09f6d8cbe93162a6f8300385b576
Paul Gillis <pauldg7@gmail.com>
2019-03-19 03:36:15 -0500

    Fixed Splash Screen Weirdness.

--- app/src/main/java/tigerhacks/android/tigerhacksapp/service/extensions/ContextExtensions.kt
+++ app/src/main/java/tigerhacks/android/tigerhacksapp/service/extensions/ContextExtensions.kt
@@ -6,5 +6,7 @@ import android.util.TypedValue
 
 fun Context.dpToPx(dp: Int) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), resources.displayMetrics).toInt()
 
+fun Context.spToPx(sp: Int) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp.toFloat(), resources.displayMetrics).toInt()
+
 @Suppress("deprecated")
 fun Context.getColorRes(@ColorRes res: Int) = resources.getColor(res)
