0df772af8e5bfa4a0a064603be083edc29cdb831
Paul Gillis <pauldg7@gmail.com>
2019-03-20 01:35:36 -0500

    Fixed a lateinit crash

--- app/src/main/java/tigerhacks/android/tigerhacksapp/sponsors/SponsorsFragment.kt
+++ app/src/main/java/tigerhacks/android/tigerhacksapp/sponsors/SponsorsFragment.kt
@@ -18,7 +18,7 @@ class SponsorsFragment : Fragment() {
         fun newInstance() = SponsorsFragment()
     }
 
-    private lateinit var layout: View
+    private var layout: View? = null
     private var home: HomeScreenActivity? = null
 
     override fun onCreateView(
@@ -37,35 +37,37 @@ class SponsorsFragment : Fragment() {
     }
 
     fun loadSponsorData(list: SponsorList?) {
-        //TODO this needs to be a recyclerView
-        val pLayout = layout.findViewById<LinearLayout>(R.id.platinumLayout)
-        val gLayout = layout.findViewById<LinearLayout>(R.id.goldLayout)
-        val sLayout = layout.findViewById<LinearLayout>(R.id.silverLayout)
-        val bLayout = layout.findViewById<LinearLayout>(R.id.bronzeLayout)
+        layout?.let { immutableLayout ->
+            val pLayout = immutableLayout.findViewById<LinearLayout>(R.id.platinumLayout)
+            val gLayout = immutableLayout.findViewById<LinearLayout>(R.id.goldLayout)
+            val sLayout = immutableLayout.findViewById<LinearLayout>(R.id.silverLayout)
+            val bLayout = immutableLayout.findViewById<LinearLayout>(R.id.bronzeLayout)
 
-        list?.sponsors?.forEach { sponsor ->
-            val image = ImageView(context)
-            val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100)
-            layoutParams.height = context!!.dpToPx(100)
-            layoutParams.setMargins(0, 32, 0, 32)
-            image.layoutParams = layoutParams
-            image.scaleType = ImageView.ScaleType.FIT_CENTER
-            Glide.with(image).load(sponsor.image).into(image)
-            when (sponsor.level) {
-                "Platinum" -> pLayout.addView(image)
-                "Gold" -> gLayout.addView(image)
-                "Silver" -> sLayout.addView(image)
-                "Bronze" -> bLayout.addView(image)
-            }
+            list?.sponsors?.forEach { sponsor ->
+                val image = ImageView(context)
+                val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100)
+                layoutParams.height = context!!.dpToPx(100)
+                layoutParams.setMargins(0, 32, 0, 32)
+                image.layoutParams = layoutParams
+                image.scaleType = ImageView.ScaleType.FIT_CENTER
+                Glide.with(image).load(sponsor.image).into(image)
+                when (sponsor.level) {
+                    "Platinum" -> pLayout.addView(image)
+                    "Gold" -> gLayout.addView(image)
+                    "Silver" -> sLayout.addView(image)
+                    "Bronze" -> bLayout.addView(image)
+                }
 
-            image.setOnClickListener {
-                startActivity(
-                    SponsorDetailActivity.newInstance(
-                        context!!,
-                        sponsor
+                image.setOnClickListener {
+                    startActivity(
+                        SponsorDetailActivity.newInstance(
+                            context!!,
+                            sponsor
+                        )
                     )
-                )
+                }
             }
         }
+
     }
 }
