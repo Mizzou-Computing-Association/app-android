f32626c3c5399a352ac0498817d9573bd57b7f14
Paul Gillis <Pauldg7@gmail.com>
2019-07-20 20:30:12 -0500

    Added FCM SNS Messaging to android app.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # app-android
 Android App for Tigerhacks
 Written in Kotlin
+
+### How to Contribute
+
+Create a SNSSecrets Object in app/src/main/kotlin/tigerhacks.android.tigerhacksapp/service/notifications/: 
+
+```Kotlin
+object SNSSecrets {
+    const val platformArn = "YOUR_PLATFORM_ARN_HERE"
+    const val identityPoolId = "YOUR_CONTIGO_POOL_ID"
+    const val topicArn = "YOUR_SUBSCRIPTION_ARN"
+}
+```
+
+Download google-services.json from Firebase console -> TigerHacksApp -> project settings and put it in app/.
---
+++ app/src/main/kotlin/tigerhacks/android/tigerhacksapp/service/notifications/TigerHacksMessagingService.kt
@@ -0,0 +1,100 @@
+package tigerhacks.android.tigerhacksapp.service.notifications
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.sns.AmazonSNSClient
+import com.amazonaws.services.sns.model.CreatePlatformEndpointRequest
+import com.amazonaws.services.sns.model.SubscribeRequest
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import tigerhacks.android.tigerhacksapp.R
+
+
+private const val TAG = "THMessagingService"
+private const val CHANNEL_ID = "tigerhacks.android.tigerhacksapp.TigerHacks"
+private const val CHANNEL_NAME = "TigerHacks"
+
+class TigerHacksMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        remoteMessage?.data?.let {
+            val message = it.values.toTypedArray()[0]
+
+            Log.d(TAG, "Message Notification Body: $message")
+
+            createChannelsIfNeeded()
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText(message)
+                .setSmallIcon(R.drawable.logo_round)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(builder.hashCode(), builder.build())
+            }
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        val credentials = CognitoCachingCredentialsProvider(
+            applicationContext,
+            SNSSecrets.identityPoolId,
+            Regions.US_EAST_1
+        )
+
+        val client = AmazonSNSClient(credentials)
+
+        val endpointArnRequest = CreatePlatformEndpointRequest()
+            .withPlatformApplicationArn(SNSSecrets.platformArn)
+            .withToken(token)
+
+        val endpointArnResult = client.createPlatformEndpoint(endpointArnRequest)
+        if (endpointArnResult == null) {
+            Log.d(TAG, "Error creating sns platform endpoint!")
+            return
+        }
+
+        val endpointArn = endpointArnResult.endpointArn
+
+        val subscribeRequest = SubscribeRequest()
+            .withProtocol("application")
+            .withEndpoint(endpointArn)
+            .withTopicArn(SNSSecrets.topicArn)
+
+        val subscribeResult = client.subscribe(subscribeRequest)
+        if (subscribeResult == null) {
+            Log.d(TAG, "Error subscribing to sns topic")
+            return
+        }
+    }
+
+    private var createdChannels = false
+
+    private fun createChannelsIfNeeded() {
+        if (createdChannels) return
+        createdChannels = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+            channel.enableLights(true)
+            channel.enableVibration(true)
+            channel.lightColor = Color.argb(100, 249, 154, 58)
+            channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager)
+                ?.createNotificationChannel(channel)
+        }
+    }
+}
+
+
