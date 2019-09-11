41e866cae6b48e67438d63cb3df49f9c2b551f69
Paul Gillis <Pauldg7@gmail.com>
2019-09-11 13:16:28 -0500

    Added Firebase auth to project, and created basic sign-in screen.

--- app/src/main/kotlin/tigerhacks/android/tigerhacksapp/SplashActivity.kt
+++ app/src/main/kotlin/tigerhacks/android/tigerhacksapp/SplashActivity.kt
@@ -3,14 +3,33 @@ package tigerhacks.android.tigerhacksapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.OAuthProvider
+import java.lang.Exception
 
 /**
  * @author pauldg7@gmail.com (Paul Gillis)
  */
+
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, HomeScreenActivity::class.java))
+        auth = FirebaseAuth.getInstance()
+
+        val nextAcitvity = if (auth.currentUser == null) {
+            // User needs to Sign-in
+            SignInActivity::class.java
+        } else {
+            // User is already signed in
+            HomeScreenActivity::class.java
+        }
+
+        startActivity(Intent(this, nextAcitvity))
         finish()
     }
 }
