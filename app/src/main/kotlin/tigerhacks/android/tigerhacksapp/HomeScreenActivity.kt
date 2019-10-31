7051096c96b6748ce60016f3c09a87f1f62b35da
Paul Gillis <Pauldg7@gmail.com>
2019-10-31 17:15:09 -0500

    Finished NavigationUI setup. Fixed laggy maps after lib update. Refactored floor maps to assets.

--- app/src/main/kotlin/tigerhacks/android/tigerhacksapp/HomeScreenActivity.kt
+++ app/src/main/kotlin/tigerhacks/android/tigerhacksapp/HomeScreenActivity.kt
@@ -2,43 +2,32 @@ package tigerhacks.android.tigerhacksapp
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.google.firebase.analytics.FirebaseAnalytics
-import kotlinx.android.synthetic.main.activity_home_screen.container
 import kotlinx.android.synthetic.main.activity_home_screen.navigationView
-import kotlinx.android.synthetic.main.activity_home_screen.tabLayout
-import kotlinx.android.synthetic.main.activity_home_screen.toolbar
-import tigerhacks.android.tigerhacksapp.prizes.PrizesFragment
-import tigerhacks.android.tigerhacksapp.help.HelpFragment
-import tigerhacks.android.tigerhacksapp.schedule.ScheduleFragment
-import tigerhacks.android.tigerhacksapp.sponsors.SponsorsFragment
-import tigerhacks.android.tigerhacksapp.maps.MapFragment
 import tigerhacks.android.tigerhacksapp.service.database.TigerHacksDatabase
 import android.os.Build
 import android.view.Menu
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import tigerhacks.android.tigerhacksapp.tigerpass.TigerPassFragment
+import kotlinx.android.synthetic.main.activity_home_screen.drawerLayout
 
 class HomeScreenActivity : AppCompatActivity() {
-    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
-    private lateinit var tigerPassFragment: TigerPassFragment
-    private lateinit var mapFragment: MapFragment
-    private lateinit var prizesFragment: PrizesFragment
-    private lateinit var scheduleFragment: ScheduleFragment
-    private lateinit var sponsorsFragment: SponsorsFragment
-    private lateinit var helpFragment: HelpFragment
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var navController: NavController
 
     lateinit var viewModel: HomeScreenViewModel
-
     lateinit var googleSignInClient: GoogleSignInClient
-    var db: TigerHacksDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -46,9 +35,6 @@ class HomeScreenActivity : AppCompatActivity() {
 
         FirebaseAnalytics.getInstance(this)
         setContentView(R.layout.activity_home_screen)
-
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.firebase_web_host))
@@ -59,74 +45,28 @@ class HomeScreenActivity : AppCompatActivity() {
 
         val db = TigerHacksDatabase.getDatabase(applicationContext)
         viewModel = ViewModelProviders.of(this, HomeScreenViewModel.FACTORY(db)).get(HomeScreenViewModel::class.java)
-        this.db = db
 
-        tigerPassFragment = TigerPassFragment.newInstance()
-        mapFragment = MapFragment.newInstance()
-        prizesFragment = PrizesFragment.newInstance()
-        scheduleFragment = ScheduleFragment.newInstance()
-        sponsorsFragment = SponsorsFragment.newInstance()
-        helpFragment = HelpFragment.newInstance()
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        val fragmentCount = supportFragmentManager.fragments.size
-        if (fragmentCount == 0) {
-            navigate(navigationView.menu.findItem(R.id.navigation_schedule))
-        } else {
-            val pos = supportFragmentManager.fragments.size - 1
-            val fragTag = supportFragmentManager.fragments[pos].tag?.split("~")
-            if (fragTag != null && fragTag.size > 1) {
-                updateTabs(fragTag[0].toInt())
-                supportActionBar?.title = fragTag[1]
-            }
-        }
+        updateNavgraphLabel()
 
-        setProfileTitle(FirebaseAuth.getInstance().currentUser != null, true)
-
-        actionBarDrawerToggle = ActionBarDrawerToggle(this, container, R.string.open, R.string.close)
-        container.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
-
-        navigationView.setNavigationItemSelectedListener(::navigate)
     }
 
-    private fun navigate(menuItem: MenuItem): Boolean {
-        updateTabs(menuItem.itemId)
-        val fragment = when (menuItem.itemId) {
-            R.id.navigation_map -> mapFragment
-            R.id.navigation_prizes -> prizesFragment
-            R.id.navigation_schedule -> scheduleFragment
-            R.id.navigation_sponsors -> sponsorsFragment
-            R.id.navigation_profile -> tigerPassFragment
-            else -> helpFragment
-        }
-        supportActionBar?.title = menuItem.title
-        supportFragmentManager
-            .beginTransaction()
-            .setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
-            .replace(R.id.contentFrameLayout, fragment, "${menuItem.itemId}~${menuItem.title}")
-            .commit()
-        return true
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    fun setProfileTitle(isProfile: Boolean, isInitial: Boolean = false) {
-        val title = if (isProfile) getString(R.string.title_profile) else getString(R.string.title_sign_in)
-        if (!isInitial) supportActionBar?.title = title
+    fun updateNavgraphLabel() {
+        val title = if (FirebaseAuth.getInstance().currentUser != null) getString(R.string.title_profile) else getString(R.string.title_sign_in)
+        navController.graph.findNode(R.id.navigation_profile)?.label = title
+
+        supportActionBar?.title = title
         navigationView.menu.findItem(R.id.navigation_profile).title = title
-    }
-
-    private fun updateTabs(fragId: Int) {
-        val list = when (fragId) {
-            R.id.navigation_map -> listOf(R.string.floor_1, R.string.floor_2, R.string.floor_3)
-            R.id.navigation_schedule -> listOf(R.string.friday, R.string.saturday, R.string.sunday)
-            else -> emptyList()
-        }
-        tabLayout.removeAllTabs()
-        for (res in list) {
-            val tab = tabLayout.newTab()
-            tab.text = getString(res)
-            tabLayout.addTab(tab)
-        }
-        tabLayout.visibility = if (list.isNotEmpty()) View.VISIBLE else View.GONE
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/kotlin/tigerhacks/android/tigerhacksapp/help/HelpFragment.kt
+++ app/src/main/kotlin/tigerhacks/android/tigerhacksapp/help/HelpFragment.kt
@@ -2,17 +2,19 @@ package tigerhacks.android.tigerhacksapp.help
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.Spinner
 import tigerhacks.android.tigerhacksapp.R
-import tigerhacks.android.tigerhacksapp.TigerApplication
-import android.widget.ArrayAdapter
-import tigerhacks.android.tigerhacksapp.ThemeMode
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import tigerhacks.android.tigerhacksapp.HomeScreenActivity
 
 //Helper data class to link View Id's to the Url that should open that that view Id is clicked
 private data class IdLink(val id: Int, val link: String)
@@ -27,7 +29,9 @@ class HelpFragment : androidx.fragment.app.Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_help, container, false)
+        val toolbarView = inflater.inflate(R.layout.toolbar_no_tab, container, false)
+        val toolbarContainer = toolbarView.findViewById<ConstraintLayout>(R.id.container)
+        val view = inflater.inflate(R.layout.fragment_help, toolbarContainer, true)
         arrayOf(
             IdLink(R.id.tigerHacksWebsiteCardView, "http://tiger-hacks.com/"),
             IdLink(R.id.mcaSlackCardView,"https://join.slack.com/t/tigerhacks2019/shared_invite/enQtNzg3ODQxMjQyNDg2LWExZTIyNWQ1ZThlMGRhMzAwNjQ4MGEwZDhhMmQxNTUwMTcyOGZiNjAxNzFkN2IzZjQxMDhhZGI5ZmFlMzkxMWQ"),
@@ -42,6 +46,16 @@ class HelpFragment : androidx.fragment.app.Fragment() {
             }
         }
 
-        return view
+        val activity = activity as HomeScreenActivity
+        val toolbar = toolbarView.findViewById<Toolbar>(R.id.toolbar)
+
+        activity.setSupportActionBar(toolbar)
+        activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navController = activity.findNavController(R.id.navHostFragment)
+        activity.setupActionBarWithNavController(navController, activity.findViewById<DrawerLayout>(R.id.drawerLayout))
+        activity.findViewById<NavigationView>(R.id.navigationView).setupWithNavController(navController)
+
+        return toolbarView
     }
 }
